4befecaf3c0ba2138bd1c83bb15b1b422e43e2b0
SarahRahal <106394289+SarahRahal@users.noreply.github.com>
2024-05-28 19:44:22 -0300

    feito a tela de menu

--- app/src/main/java/com/example/boaviagemsarah/screens/Menu.kt
+++ app/src/main/java/com/example/boaviagemsarah/screens/Menu.kt
@@ -6,7 +6,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,26 +23,97 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+private fun isSelected(currentDestination: NavDestination?, route:String): Boolean {
+    return currentDestination?.hierarchy?.any {it.route == route} == true
+}
+
+@Composable
+fun Home() {
+    Column {
+        Text(text = "home")
+    }
+
+}
 
 @Composable
 fun Menu(onBack: () ->Unit){
 
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.LightGray)
-
-    ){
+    val navController = rememberNavController()
 
 
+
+    Scaffold (
+        bottomBar = {
+            val navBackStackEntry = navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.value?.destination
+
+            BottomNavigation {
+
+                BottomNavigationItem(
+                    selected = isSelected(currentDestination, "home"),
+                    onClick = { navController.navigate("home") },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Home,
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                BottomNavigationItem(
+                    selected = isSelected(currentDestination, "viagem"),
+                    onClick = { navController.navigate("viagem") },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = ""
+                        )
+                    }
+                )
+
+                BottomNavigationItem(
+                    selected = isSelected(currentDestination, "sobre"),
+                    onClick = { navController.navigate("sobre") },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Settings,
+                            contentDescription = ""
+                        )
+                    }
+                )
+            }
         }
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+
+        ){
+            NavHost(
+                navController = navController,
+                startDestination = "home"
+            ) {
+                composable("home") {
+                    Home()
+                }
+                composable("viagem"){
+                    Viagens()
+                }
+
+                composable("sobre"){
+                    Sobre()
+                }
 
 
+            }
+        }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewMenu() {
-    Menu({})
 }
