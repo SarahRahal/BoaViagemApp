a79f7b9a5655d4c93260aa21594edfae226b0d45
SarahRahal <106394289+SarahRahal@users.noreply.github.com>
2024-05-28 19:46:44 -0300

    feito as outras duas telas e a navegação do menu

---
+++ app/src/main/java/com/example/boaviagemsarah/screens/Viagens.kt
@@ -0,0 +1,16 @@
+package com.example.boaviagemsarah.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Viagens(){
+    Column (
+        modifier = Modifier
+
+    ){
+        Text(text = "Cadastrar uma viagem")
+    }
+}
